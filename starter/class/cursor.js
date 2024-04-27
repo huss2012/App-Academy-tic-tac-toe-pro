d88d0116391c8edd07fba5ee780299060def5f65
huss2012 <huss24940@gmail.com>
2024-04-27 15:19:38 +0300

    Curser problems have been solved.

--- starter/class/cursor.js
+++ starter/class/cursor.js
@@ -1,4 +1,8 @@
+const keypress = require("keypress");
 const Screen = require("./screen");
+
+//Listen for keypress Events:
+keypress(process.stdin);
 
 class Cursor {
 
@@ -12,6 +16,15 @@ class Cursor {
     this.gridColor = 'black';
     this.cursorColor = 'yellow';
 
+    process.stdin.on('keypress', (ch, key) => {
+      if (key) {
+        if (key.name === "up") this.up();
+        else if (key.name === "down") this.down();
+        else if (key.name === "left") this.left();
+        else if (key.name === "right") this.right();
+      }
+    });
+
   }
 
   resetBackgroundColor() {
@@ -24,18 +37,38 @@ class Cursor {
 
   up() {
     // Move cursor up
+    if (this.row > 0) {
+      this.resetBackgroundColor();
+      this.row--;
+      this.setBackgroundColor();
+    }
   }
 
   down() {
     // Move cursor down
+    if (this.row < this.numRows - 1) {
+      this.resetBackgroundColor();
+      this.row++;
+      this.setBackgroundColor();
+    }
   }
 
   left() {
     // Move cursor left
+    if (this.col > 0) {
+      this.resetBackgroundColor();
+      this.col--;
+      this.setBackgroundColor();
+    }
   }
 
   right() {
     // Move cursor right
+    if (this.col < this.numCols - 1) {
+      this.resetBackgroundColor();
+      this.col++;
+      this.setBackgroundColor();
+    }
   }
 
 }
